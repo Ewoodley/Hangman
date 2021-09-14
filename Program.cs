e188ad85d6980a68c722ca830399bf3aea46e6f0
Eric Woodley <noctisumbra@gmail.com>
2021-09-14 18:40:25 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Hangman
+{
+    class Program
+    {
+        //private List<WordList>
+
+        static void Main(string[] args)
+        {
+            List<string> wordlist = new List<string>()
+            {
+
+              "Coding", "Microsoft", "Hello"
+            };
+            //will be implimenting in a class
+
+            Random getanswer = new Random();
+            var newword = getanswer.Next(wordlist.Count);
+            string word = wordlist[newword];
+            char[] answer = word.ToCharArray();
+            int length = word.Length;
+            var guesscount = 6;
+            int guesses;
+            char[] response = new char [answer.Length];
+            //char input;
+            
+
+
+
+
+            Console.WriteLine("Welcome to Hangman!");
+            Console.WriteLine($"In order to win, you must guess by entering letters. You have {guesscount} wrong guesses remaining.");
+            Console.WriteLine($"The word has: {length} letters.");
+            Console.WriteLine("Please enter your first guess:");
+
+            guesses = 0;
+            while(guesscount > guesses)
+            {
+                
+                foreach (char input in answer)
+                {
+                    
+                    for (var g = 0; g < answer.Length; g++) 
+                    {
+                        //int i;
+                        //response[] = g;
+                    
+                    //for (var i = 0; i < response.Length[g]; i++) ; //this is ment to contain the user input and place it into an array to compare to the answer array
+                        
+                            
+
+                            response[g] = Convert.ToChar(Console.ReadLine());
+                            if (response[g] == input)
+                            {
+                                Console.WriteLine("Correct, please choose another letter");
+                            }
+                            else
+                            {
+                                guesscount--;
+                                Console.WriteLine($"Incorrect, you have {guesscount} guesses remaining");
+
+                            }
+                        
+                    }
+
+                }
+
+            }
+            
+                            
+
+
+         
+            
+            }
+
+    }
+}
---
+++ WordList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+namespace Hangman
+{
+    class WordList
+    {
+        private List<string> wordlist;
+        
+            public WordList()
+            { 
+            wordlist = new List<string>()
+                {
+              "Coding", "Microsoft", "Hello",
+               
+                };
+
+            public string NewWord()
+            {
+                var random = new Random();
+                var word = random.Next();
+                return wordlist[word];
+            }
+            }
+    }
+}
+*/
